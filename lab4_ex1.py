eba1ef9658716f5d2e5d56ff0a6a09d46b669fd2
baksom <72859411+baksom@users.noreply.github.com>
2020-11-12 16:37:54 +0200

    Add files via upload
    сделали1 задание

---
+++ lab4_ex1.py
@@ -0,0 +1,11 @@
+def power(a, n):
+    if n == 0:
+        return 1
+    res = power(a * a, n // 2)
+    if n % 2:
+        res *= a
+    return res
+ 
+a = float(input())
+n = float(input())
+print(power(a, n))
