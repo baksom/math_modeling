c66df50f96974771c983ef162216573de9cf92ee
baksom <72859411+baksom@users.noreply.github.com>
2020-11-26 19:10:01 +0200

    Add files via upload

---
+++ lab6_ex1.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.zeros(shape = 5)
+y = np.zeros(shape = 5)
+print("Введите первую координату:", sep='')
+x[0] = input("Ось X ")
+y[0] = input("Ось Y ")
+a = float(input("Введите размер стороны: ",))
+x = [x[0],x[0]+a,x[0]+a,x[0],x[0]]
+y = [y[0],y[0],y[0]+a,y[0]+a,x[0]]
+
+plt.plot(x,y)
+plt.show
